371f0509b53d4274f1a7b9392fe9de1a2064d960
buzruk <buzurgmexrs@gmail.com>
2024-05-02 22:10:23 +0500

    Fix controller namespace

--- Task5/Solution/Controllers/FakeDataController.cs
+++ Task5/Solution/Controllers/FakeDataController.cs
@@ -6,52 +6,50 @@ using FakeGeneration.Services;
 using CsvHelper;
 using System.Globalization;
 
-namespace YourAppName.Controllers
-{
-    public class FakeDataController : Controller
-    {
-        private readonly IDataGeneration _generator;
-        public FakeDataController(IDataGeneration generator)
-        {
-            _generator = generator;
-            
-        }
+namespace FakeGeneration.Controllers;
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+  public class FakeDataController : Controller
+  {
+      private readonly IDataGeneration _generator;
+      public FakeDataController(IDataGeneration generator)
+      {
+          _generator = generator;
+          
+      }
 
-        public IActionResult GenerateData(double errorCount, int seed, int count, string region = "en")
-        {   
-            var users = _generator.GetData(seed, region, count);
-            var errorsLogic = new ErrorService(seed);
-            for (int i = 0; i < users.Length; i++)
-            {
-                int errorCounts = errorsLogic.CalculateCountOfErrors(errorCount);
-                for (int j = 0; j < errorCounts; j++)
-                {
-                   users[i] = errorsLogic.ErrorActivation(users[i]);
-                }
-            }
-            return Ok(users);
-        }
-        [HttpPost]
-        public IActionResult CreateCsv([FromBody]IEnumerable<User> users)
-        {
-            var path = $"{Directory.GetCurrentDirectory()}{DateTime.Now.Ticks}.csv";
+      public IActionResult Index()
+      {
+          return View();
+      }
 
-            using var writer = new StreamWriter(path);
+      public IActionResult GenerateData(double errorCount, int seed, int count, string region = "en")
+      {   
+          var users = _generator.GetData(seed, region, count);
+          var errorsLogic = new ErrorService(seed);
+          for (int i = 0; i < users.Length; i++)
+          {
+              int errorCounts = errorsLogic.CalculateCountOfErrors(errorCount);
+              for (int j = 0; j < errorCounts; j++)
+              {
+                 users[i] = errorsLogic.ErrorActivation(users[i]);
+              }
+          }
+          return Ok(users);
+      }
+      [HttpPost]
+      public IActionResult CreateCsv([FromBody]IEnumerable<User> users)
+      {
+          var path = $"{Directory.GetCurrentDirectory()}{DateTime.Now.Ticks}.csv";
 
-            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
-            {
-                csv.WriteRecords(users);
-            }
+          using var writer = new StreamWriter(path);
 
-            return PhysicalFile(path, "text/csv");
-        }
-    }
+          using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+          {
+              csv.WriteRecords(users);
+          }
 
-   
+          return PhysicalFile(path, "text/csv");
+      }
+  }
 
-}
+ 
