77f531206bd7ff9e19bddb202bdb3d0de4552f52
buzruk <buzurgmexrs@gmail.com>
2024-05-02 20:46:50 +0500

    Solve Task 5

--- Task5/Solution/GlobalUsings.cs
+++
@@ -1,9 +0,0 @@
-﻿#region Using
-global using CsvHelper;
-global using CsvHelper.Configuration;
-global using System.Globalization;
-global using Microsoft.JSInterop;
-global using Bogus;
-global using Solution.Generators;
-global using Solution.Data;
-#endregion
